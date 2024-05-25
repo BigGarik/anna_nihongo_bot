9d1f44f3b2b1427d6971e19fce3f389436d1c1d3
Игорь <inetsmol@gmail.com>
2024-05-25 15:09:35 +0300

    админка

---
+++ handlers/admin_handlers.py
@@ -0,0 +1,17 @@
+import logging
+
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+# Инициализируем роутер уровня модуля
+router = Router()
+
+logger = logging.getLogger(__name__)
+
+
+# Этот хэндлер срабатывает на команду /help
+@router.message(Command(commands='settings'), F.from_user.id.in_({815174734, 693131974}))
+async def process_help_command(message: Message):
+    await message.answer(text='/settings')
