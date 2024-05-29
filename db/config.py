a496d6f81509eb662d1408998f9999445b55f71b
Игорь <inetsmol@gmail.com>
2024-05-29 09:53:10 +0300

    models.tts

--- db/config.py
+++ db/config.py
@@ -4,7 +4,7 @@ from tortoise import Tortoise
 async def init(db_url):
     await Tortoise.init(
         db_url=db_url,
-        modules={'models': ['models.user', 'models.phrase']}
+        modules={'models': ['models.user', 'models.phrase', 'models.tts']}
     )
     await Tortoise.generate_schemas()
 
