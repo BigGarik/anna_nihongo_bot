4d2616c6fc4506358fcd0a533115f160053c7b70
Игорь <inetsmol@gmail.com>
2024-05-29 10:00:03 +0300

    voice_id = fields.CharField

--- models/tts.py
+++ models/tts.py
@@ -3,7 +3,7 @@ from tortoise import fields, models
 
 class TextToSpeech(models.Model):
     id = fields.IntField(pk=True)
-    voice_id = fields.IntField()
+    voice_id = fields.CharField(max_length=255)
     user_id = fields.IntField()
     text = fields.CharField(max_length=255, unique=True)
     voice = fields.BinaryField()
