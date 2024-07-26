2afacb8afc61aa00a04c58ff4bb4a1d4ce24e44b
Игорь <inetsmol@gmail.com>
2024-07-26 12:59:29 +0300

    Поиск и удаление ключа в Redis

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,10 +1,12 @@
 import logging
+import re
 
 from aiogram import Router
 from aiogram.exceptions import TelegramAPIError
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message, Update, ErrorEvent
 
+from bot_init import redis
 from lexicon.lexicon_ru import LEXICON_RU
 
 # Инициализируем роутер уровня модуля
@@ -28,4 +30,19 @@ async def send_echo(message: Message, state: FSMContext):
 @router.error()
 async def error_handler(event: ErrorEvent):
     logger.critical("Critical error caused by %s", event.exception, exc_info=True)
-    # do something with error
+    if "Context not found for intent id" in str(event.exception):
+        # Извлекаем ID интента из сообщения об ошибке
+        intent_id_match = re.search(r'intent id: (\w+)', str(event.exception))
+        if intent_id_match:
+            intent_id = intent_id_match.group(1)
+
+            # Поиск и удаление ключа в Redis
+            for key in redis.scan_iter("*"):
+                value = redis.get(key)
+                if value and intent_id.encode() in value:
+                    redis.delete(key)
+                    logger.info(f"Удален ключ из Redis: {key}")
+                    break
+            else:
+                logger.warning(f"Ключ для intent id {intent_id} не найден в Redis")
+
